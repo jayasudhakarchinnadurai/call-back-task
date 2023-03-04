9e8f99f864580bb41d72838e7793fcf755359aee
jayasudhakarchinnadurai <jayasudhakar183@gmail.com>
2023-03-04 12:43:01 +0530

    complete

---
+++ script.js
@@ -0,0 +1,56 @@
+setTimeout(()=>{
+setTimeout(()=>{
+setTimeout(()=>{  
+setTimeout(()=>{
+setTimeout(()=>{
+setTimeout(()=>{
+setTimeout(()=>{ 
+setTimeout(()=>{
+setTimeout(()=>{
+setTimeout(()=>{
+setTimeout(()=>{
+var data=document.querySelector('body')
+data.innerText='Happy Independence Day'
+data.style.color='red'
+  },1000)
+var data=document.querySelector('body')
+data.innerText='1'
+data.style.color='blue'
+    },1000)
+    var data=document.querySelector('body')
+data.innerText='2'
+data.style.color='green'
+},1000)
+var data=document.querySelector('body')
+data.innerText='3'
+data.style.color='pink'
+},1000)
+var data=document.querySelector('body')
+data.innerText='4'
+data.style.color='yellow'
+
+},1000)
+var data=document.querySelector('body')
+data.innerText='5'
+data.style.color='black'
+},1000)
+var data=document.querySelector('body')
+data.innerText='6'
+data.style.color='blue'
+},1000)
+var data=document.querySelector('body')
+data.innerText='7'
+data.style.color='violet'
+},1000)
+var data=document.querySelector('body')
+data.innerText='8'
+data.style.color='pink'
+},1000)
+var data=document.querySelector('body')
+data.innerText='9'
+data.style.color='green'
+},1000)
+var data=document.querySelector('body')
+data.innerText='10'
+data.style.color='blue'
+} ,1000)
